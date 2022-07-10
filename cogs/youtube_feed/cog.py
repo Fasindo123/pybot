cef99bd6697d31338282c972dc7279dab4ec0f1c
matethelol <74656862+Fasindo123@users.noreply.github.com>
2022-07-10 23:22:53 +0200

    1.0
    igen

---
+++ cogs/youtube_feed/cog.py
@@ -0,0 +1,48 @@
+import config
+import nextcord
+from nextcord.ext import commands
+from nextcord.ext.tasks import loop
+
+from .youtube_feed import YouTubeFeed
+
+CHECK_INTERVAL = 60  # number of seconds before checking again
+
+
+class YouTubeFeedCog(commands.Cog, name="YouTube Feed"):
+    def __init__(self, bot: commands.Bot):
+        self.__bot = bot
+        self.__feed = YouTubeFeed(config.YT_CHANNEL_ID)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """When discord is connected"""
+        # get channel object
+        self.__channel: nextcord.TextChannel = self.__bot.get_channel(
+            config.YOUTUBE_VIDEOS_CHANNEL_ID
+        )
+        assert isinstance(self.__channel, nextcord.TextChannel)
+        # get role
+        self.__youtube_ping_role: nextcord.Role = self.__channel.guild.get_role(
+            config.YOUTUBE_PING_ROLE_ID
+        )
+        assert isinstance(self.__youtube_ping_role, nextcord.Role)
+        # check that channel exists
+        assert isinstance(self.__channel, nextcord.TextChannel)
+        # start YouTube feed
+        self.feed_loop.start()
+        print("YouTube feed started")
+
+    @loop(seconds=CHECK_INTERVAL)
+    async def feed_loop(self):
+        """Loop to check YouTube and post new videos"""
+        # check for new video
+        if not self.__feed.has_new_video():
+            return
+        video = self.__feed.get_most_recent_video()
+        await self.__channel.send(
+            f"{self.__youtube_ping_role.mention} **{video.author}** just posted a video! Go check it out!\n{video.link}"
+        )
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(YouTubeFeedCog(bot))
---
+++ cogs/youtube_feed/youtube_feed.py
@@ -0,0 +1,31 @@
+import time
+import feedparser
+
+from .video import Video
+
+
+class YouTubeFeed:
+    """Feed for checking for new videos in a YouTube channel"""
+
+    def __init__(self, channel_id: str):
+        self.__last_publish_date = time.gmtime(0)
+        self.__channel_id = channel_id
+        self.update_feed()
+
+    def update_feed(self):
+        """Parse the feed again and update the last update id"""
+        self.__feed = feedparser.parse(
+            f"https://www.youtube.com/feeds/videos.xml?channel_id={self.__channel_id}"
+        )
+        most_recent = self.get_most_recent_video()
+        self.__last_publish_date = max(self.__last_publish_date, most_recent.published)
+
+    def get_most_recent_video(self) -> Video:
+        """return first entry in feed"""
+        return Video(self.__feed.entries[0])
+
+    def has_new_video(self) -> bool:
+        """check if the last update is newer than what it was previously"""
+        prev_last_publish_date = self.__last_publish_date
+        self.update_feed()
+        return self.__last_publish_date > prev_last_publish_date
